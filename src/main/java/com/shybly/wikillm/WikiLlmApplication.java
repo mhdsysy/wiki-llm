0afc9f184bfe894844f4b51fdcdc06cf47fd71be
Mohamad Shybly <mohamad.shybly@mathplan.de>
2024-08-16 15:36:30 +0300

    Project initialisation

---
+++ src/main/java/com/shybly/wikillm/WikiLlmApplication.java
@@ -0,0 +1,13 @@
+package com.shybly.wikillm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiLlmApplication.class, args);
+	}
+
+}
